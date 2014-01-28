a35845145dfff823193644caaa631c47d75819e3
boorasunilkumar <boorasunilkumar@gmail.com>
2014-01-28 15:42:25 +1000

    upload.php
    with restrictions

--- upload.php
+++ upload.php
@@ -1,4 +1,17 @@
 <?php
+$allowedexts = array("pdf","jpg","doc","txt","jpeg","gif");
+$temp=explode(".",$_FILES["file"]["name"]);
+$ext=end($temp);
+if ((
+($_FILES["file"]["type"]=="application/pdf")
+||($_FILES["file"]["type"]=="text/plain")
+||($_FILES["file"]["type"]=="image/jpg")
+||($_FILES["file"]["type"]=="image/jpeg")
+||($_FILES["file"]["type"]=="image/gif")
+||($_FILES["file"]["type"]=="application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
+&&($_FILES["file"]["size"]<4000000)
+&&in_array($ext,$allowedexts))
+{
   if ($_FILES["file"]["error"] > 0)
     {
     echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
@@ -21,6 +34,10 @@
       echo "Stored in: " . "C:/xampp/tmp/uploads/" . $_FILES["file"]["name"];
       }
     }
-  
+  }
+  else
+  {
+  echo "unsupported file";
+  }
 
 ?>
